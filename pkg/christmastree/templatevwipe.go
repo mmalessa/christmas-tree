f338bd0ac1ac8aed4909edee9873eba28898511d
Marcin Malessa <marcin@malessa.pl>
2021-12-31 16:52:13 +0100

    Modify templatevwipe

--- pkg/christmastree/templatevwipe.go
+++ pkg/christmastree/templatevwipe.go
@@ -8,24 +8,39 @@ import (
 config:
 	colors: [0xRRGGBB, 0xRRGGBB]	// wipe color
 	tick: 100						// sleep time (ms)
+	direction: 0					// 0 or 1 (1==reverse)
 */
 func (ch *ChristmasTree) PlayTemplateVWipe(config map[string]interface{}) error {
 	colors := config["colors"].([]interface{})
 	tick := config["tick"].(int)
+	direction := config["direction"].(int)
 	rows := ch.treeconfig["rows"].([]interface{})
+	countrows := len(rows)
+
+	colorRow := func(color int, rowrange map[interface{}]interface{}) error {
+		min := rowrange["min"].(int)
+		max := rowrange["max"].(int)
+		for i := min; i <= max; i++ {
+			ch.dev.Leds(0)[i] = uint32(color)
+		}
+		if err := ch.dev.Render(); err != nil {
+			return err
+		}
+		time.Sleep(time.Duration(tick) * time.Millisecond)
+		return nil
+	}
 
 	for _, color := range colors {
-		for _, row := range rows {
-			rowrange := row.(map[interface{}]interface{})
-			min := rowrange["min"].(int)
-			max := rowrange["max"].(int)
-			for i := min; i <= max; i++ {
-				ch.dev.Leds(0)[i] = uint32(color.(int))
+		if direction == 0 {
+			for i := 0; i < countrows; i++ {
+				row := rows[i]
+				colorRow(color.(int), row.(map[interface{}]interface{}))
 			}
-			if err := ch.dev.Render(); err != nil {
-				return err
+		} else {
+			for i := countrows - 1; i >= 0; i-- {
+				row := rows[i]
+				colorRow(color.(int), row.(map[interface{}]interface{}))
 			}
-			time.Sleep(time.Duration(tick) * time.Millisecond)
 		}
 	}
 	return nil
